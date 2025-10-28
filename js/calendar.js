cbd781630e382f3ef77f2410385a300f818a1595
Halima Habib <45460979+habibzeeh@users.noreply.github.com>
2025-10-28 17:28:32 +0000

    Create calendar.js

---
+++ js/calendar.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const fc = window.FullCalendar;
+
+  const cal = new fc.Calendar(document.getElementById('calendar'), {
+    timeZone: 'Europe/London',
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listMonth'
+    },
+    eventSources: [
+      {
+        url: 'https://ics.habibecakery.co',
+        format: 'ics'
+      }
+    ]
+  });
+
+  cal.render();
+});
